6e1e3f1733013e138b9adaceb2b7138b96ee8d2b
Viren <viren@xyz.com>
2020-08-11 23:25:35 +0530

    Added views to create/edit ---without static or boottrap files

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,12 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-path('', views.post_list_view, name='post_list')
+	path('', views.post_list_view, name='post_list'),
+
+	path('post/<int:pk>/', views.post_detail_view, name='post_detail'),
+
+	path('post/new/', views.post_new_view, name='post_new'),
+	# added to remove runtime error it work what is the diffrence in follwing code ?
+	#path('post/edit/<int:pk>/', views.post_edit_view, name='post_edit'),
+  	path('post/<int:pk>/edit/', views.post_edit_view, name='post_edit'),
 ]
